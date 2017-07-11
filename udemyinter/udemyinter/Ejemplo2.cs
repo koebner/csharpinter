0f4af01884e9dfe687bbaab00eedfcd68bcc45af
Isaac Marquina Moctezuma <auzpit@hotmail.com>
2017-07-11 13:04:59 -0500

    mas ejercicios

---
+++ udemyinter/udemyinter/Ejemplo2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace udemyinter
+{
+    class Ejemplo2
+    {
+        private int _uno, _dos, _tres;
+
+        public void Metodo1(int numerito)
+        {
+            _uno = numerito;
+
+
+        }
+
+        public void Metodo2(string argumento1)
+        {
+
+            this._dos = 45465;
+            Console.WriteLine(argumento1 + (_dos));
+        }
+
+    }
+}
---
+++ udemyinter/udemyinter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace udemyinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Soy una consola mama");
+        }
+    }
+}
